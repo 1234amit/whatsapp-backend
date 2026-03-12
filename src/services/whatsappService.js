e52efca544155b208db0fcbe7828d8ab43c7f46b
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2026-03-12 20:49:36 +0600

    update

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,10 +1,53 @@
+// const { Client, LocalAuth } = require("whatsapp-web.js");
+// const qrcode = require("qrcode-terminal");
+
+// let client;
+
+// function initWhatsApp(io) {
+
+//   console.log("Initializing WhatsApp client...");
+
+//   client = new Client({
+//     authStrategy: new LocalAuth({
+//       clientId: "whatsapp-session"
+//     }),
+//     puppeteer: {
+//       headless: false,
+//       args: ["--no-sandbox"]
+//     }
+//   });
+
+//   client.on("qr", (qr) => {
+//     console.log("QR RECEIVED - Scan with WhatsApp");
+//     qrcode.generate(qr, { small: true });
+
+//     if (io) io.emit("qr", qr);
+//   });
+
+//   client.on("authenticated", () => {
+//     console.log("WhatsApp authenticated!");
+//   });
+
+//   client.on("ready", () => {
+//     console.log("WhatsApp client ready!");
+//   });
+
+//   client.on("disconnected", (reason) => {
+//     console.log("WhatsApp disconnected:", reason);
+//   });
+
+//   client.initialize();
+// }
+
+// module.exports = { initWhatsApp };
+
+
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 let client;
 
 function initWhatsApp(io) {
-
   console.log("Initializing WhatsApp client...");
 
   client = new Client({
@@ -12,7 +55,7 @@ function initWhatsApp(io) {
       clientId: "whatsapp-session"
     }),
     puppeteer: {
-      headless: false,
+      headless: true,
       args: ["--no-sandbox"]
     }
   });
@@ -20,8 +63,6 @@ function initWhatsApp(io) {
   client.on("qr", (qr) => {
     console.log("QR RECEIVED - Scan with WhatsApp");
     qrcode.generate(qr, { small: true });
-
-    if (io) io.emit("qr", qr);
   });
 
   client.on("authenticated", () => {
@@ -32,11 +73,12 @@ function initWhatsApp(io) {
     console.log("WhatsApp client ready!");
   });
 
-  client.on("disconnected", (reason) => {
-    console.log("WhatsApp disconnected:", reason);
-  });
-
   client.initialize();
 }
 
-module.exports = { initWhatsApp };
+async function sendMessage(number, message) {
+  const chatId = number + "@c.us";
+  return await client.sendMessage(chatId, message);
+}
+
+module.exports = { initWhatsApp, sendMessage };
